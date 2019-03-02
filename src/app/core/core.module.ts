346b8ac10b108bf7e504dc35aa28e2301a2a516c
João Pedro Aquino <whisoer@MacBook-Pro-de-Joao.local>
2019-03-02 01:26:58 -0300

    feat: adding material module;

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,19 @@
+import 'hammerjs';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from './components/material.module';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    BrowserAnimationsModule,
+    CommonModule,
+    MaterialModule
+  ],
+  exports: [
+    BrowserAnimationsModule,
+    MaterialModule
   ]
 })
 export class CoreModule { }
