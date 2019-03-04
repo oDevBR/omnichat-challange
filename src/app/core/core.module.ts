86a66f5ef582f1ffd831d1694d07c8c7ba248144
João Pedro Aquino <whisoer@MacBook-Pro-de-Joao.local>
2019-03-04 00:21:58 -0300

    fix: httpClienteModule import error;

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,7 +3,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ModuleWithProviders } from '@angular/compiler/src/core';
 import { ErrorInterceptor, AuthenticationInterceptor } from './helpers';
 import { AlertService, CustomerService } from './services';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 export const HELPERS = [ErrorInterceptor, AuthenticationInterceptor];
 
@@ -12,10 +12,12 @@ export const SERVICES = [AlertService, CustomerService];
 @NgModule({
   declarations: [],
   imports: [
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   exports: [
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ]
 })
 export class CoreModule { 
