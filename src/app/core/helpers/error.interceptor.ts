c2f415fcf6ff7b57c53b4b3a44e54f175a58d7fa
João Pedro Aquino <whisoer@MacBook-Pro-de-Joao.local>
2019-03-03 23:46:19 -0300

    feat: add error interceptor;

---
+++ src/app/core/helpers/error.interceptor.ts
@@ -0,0 +1,25 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { AlertService } from '../services';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+    constructor(private alert: AlertService) { }
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(request).pipe(catchError(err => {
+            let error = err.error.message || err.statusText;
+            console.log('Error code: ', err.status)
+            
+            if (err.status === 404) {
+                this.alert.swalError(error)
+            } else {
+                this.alert.swalError('Ocorreu um erro inexperado!')
+            }
+            return throwError(error);
+        }))
+    }
+}
