6391b70f9e75469d9893fcab431ec44ed198e10d
João Pedro Aquino <whisoer@MacBook-Pro-de-Joao.local>
2019-03-04 00:24:16 -0300

    refactor: change common module in 'customers.module' to shared module;

--- src/app/pages/customers/customers.module.ts
+++ src/app/pages/customers/customers.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomerListComponent } from './customer-list/customer-list.component';
 import { CustomerFormComponent } from './customer-form/customer-form.component';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [CustomerListComponent, CustomerFormComponent],
   imports: [
-    CommonModule,
+    SharedModule,
     CustomersRoutingModule
   ]
 })
