652b4a34a84ece73c67f19f76e3be59465791945
João Pedro Aquino <whisoer@MacBook-Pro-de-Joao.local>
2019-03-04 21:53:58 -0300

    feat: add new calls in shared module;

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from './components/material.module';
+import { NgxPaginationModule } from "ngx-pagination";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
-    MaterialModule
+  CommonModule,
+    MaterialModule,
+    NgxPaginationModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule
   ],
   exports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    NgxPaginationModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule
   ]
 })
 export class SharedModule { }
