5fbc3fcbe2fff1a4abc746ecd7d3a553c94ab75f
João Pedro Aquino <whisoer@MacBook-Pro-de-Joao.local>
2019-03-04 21:52:06 -0300

    fix: CORS authentication problems;

--- src/app/core/helpers/authentication.interceptor.ts
+++ src/app/core/helpers/authentication.interceptor.ts
@@ -7,6 +7,9 @@ export class AuthenticationInterceptor implements HttpInterceptor {
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         request = request.clone({
             setHeaders: {
+                'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With',
+                'Access-Control-Allow-Methods': 'DELETE, POST, GET, OPTIONS',
+                'Access-Control-Allow-Origin': '*',
                 'x-api-key': 'zAuGBFw9QX1mTCrIjPMXpaG0PgziOEEt2uYEGwt4',
                 'x-api-secret': 'r:e86306413b50cbc682d784cfd3a2f20f'
             }
