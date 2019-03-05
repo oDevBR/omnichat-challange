05a36327905a419170c95b761bf8450780f2863a
João Pedro Aquino <whisoer@MacBook-Pro-de-Joao.local>
2019-03-04 21:49:59 -0300

    feat: add customer list operations;

--- src/app/pages/customers/customer-list/customer-list.component.ts
+++ src/app/pages/customers/customer-list/customer-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Customer } from '../../../shared/models/customer';
+import { CustomerService } from '../../../core/services';
+import { MatDialogConfig, MatDialog, MatPaginator } from '@angular/material';
+import { CustomerFormComponent } from '../customer-form/customer-form.component';
 
 @Component({
   selector: 'app-customer-list',
@@ -6,10 +10,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-list.component.scss']
 })
 export class CustomerListComponent implements OnInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor() { }
-
-  ngOnInit() {
+  dataSource: any;
+  columnsToDisplay = ['name', 'mail', 'company', 'action'];
+  expandedElement: Customer | null;
+  page: number = 1;
+  limitPerPage: number = 10;
+  length: number = 0;
+  customer: Customer = {
+    objectId: '',
+    createdAt: '',
+    updatedAt: '',
+    name: '',
+    lastName: '',
+    acceptsMarketing: false,
+    email: '',
+    phoneAreaCode: '',
+    phoneCountryCode: '',
+    phoneNumber: '',
+    birthDate: '',
+    businessAccount: false,
+    businessName: '',
+    address: {
+      addressLine1: '',
+      addressLine2: '',
+      number: '',
+      suburb: '',
+      city: '',
+      state: '',
+      country: '',
+      zip: ''
+    }
   }
 
+  constructor(
+    private customerService: CustomerService,
+    private dialog: MatDialog
+    ) { }
+
+  ngOnInit() { 
+    this.getAllCustomers()
+  }
+
+  getAllCustomers(page?: number) {
+    if (page === undefined) {
+      page = 1;
+    }
+    this.page = page;
+
+    this.customerService.getAll(page, this.limitPerPage).subscribe(res => {
+      this.dataSource = res;
+      this.dataSource.paginator = this.paginator;
+    })
+    this.getCountCustomers()
+  }
+
+  getCountCustomers() {
+    this.customerService.count().subscribe(res => {
+      this.length = +(res.count / 10)
+   })
+  }
+
+  private openDialog(customer) {
+    if(customer == null) {
+      customer = this.customer;
+    }
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = customer;
+    this.dialog.open(CustomerFormComponent, dialogConfig);
+  }
 }
